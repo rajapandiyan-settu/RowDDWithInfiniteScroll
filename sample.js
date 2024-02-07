bc60905dd230fca4c49ef2c13410f5fefb9809f7
Rajapandiyan Settu <rajapandiyan.settu@syncfusion.com>
2024-02-07 11:15:03 +0530

    update sample

--- sample.js
+++ sample.js
@@ -14,6 +14,13 @@ var editmode = [
     { id: 'Batch', mode: 'Batch' },
     { id: 'Dialog', mode: 'Dialog' }
 ];
+
+
+var remoteData = new ej.data.DataManager({
+    url: 'https://services.odata.org/V4/Northwind/Northwind.svc/Orders/',
+    adaptor: new ej.data.ODataV4Adaptor
+});
+
 var grid = new ej.grids.Grid({
     dataSource: window.orderData,
     height: 300,
@@ -43,7 +50,7 @@ var grid = new ej.grids.Grid({
         { field: 'CustomerID', headerText: 'Customer ID', width: 120, editType: 'dropdownedit' },
         { field: 'Freight', headerText: 'Frieght', width: 120, format: 'C2', editType: 'numericedit', textAlign: 'Right' },
         { field: 'OrderDate', headerText: 'Order Date', width: 120, format: 'yMd', editType: 'datepickeredit', textAlign: 'Right' },
-        { field: 'ShipCity', headerText: 'Ship City', width: 120, validationRules: { required: true, minLength: 3, maxLength: 20 } },
+        { field: 'ShipCity', headerText: 'Ship City', width: 120, template: '<button>${ShipCity}</button>',  validationRules: { required: true, minLength: 3, maxLength: 20 } },
         { field: 'ShipName', headerText: 'Ship Name', width: 120, clipMode: 'EllipsisWithTooltip' },
         { field: 'ShipCountry', headerText: 'Ship Country', width: 120 },
         { field: 'ShipRegion', headerText: 'Ship Region', width: 120, type: 'string' },
@@ -113,7 +120,7 @@ var dropDownFilterType = new ej.dropdowns.DropDownList({
     dataSource: filtertype,
     fields: { text: 'type', value: 'id' },
     value: 'Excel',
-    width: 200,
+    width: 100,
     change: function (e) {
         var dropSelectedValue = e.value;
         grid.filterSettings.type = dropSelectedValue;
@@ -122,27 +129,15 @@ var dropDownFilterType = new ej.dropdowns.DropDownList({
 });
 dropDownFilterType.appendTo('#filterType');
 
-// grid.toolbarClick = function (args) {
-//     if (args.item.id === 'Grid_pdfexport') {
-//         grid.pdfExport();
-//     }
-//     if (args.item.id === 'Grid_excelexport') {
-//         grid.excelExport();
-//     }
-//     if (args.item.id === 'Grid_csvexport') {
-//         grid.csvExport();
-//     }
-// };
-
 var checkBoxObj = new ej.buttons.CheckBox({ label: 'Enable RTL', change: function (e) {
     grid.enableRtl = e.checked;
 } });
 checkBoxObj.appendTo('#rtlmode');
 
-var enableChk = new ej.buttons.CheckBox({ label: 'Enable Checkbox column', change: function (args) {
+var enableChk = new ej.buttons.CheckBox({ label: 'Checkbox column', change: function (args) {
     if (args.checked) {
         grid.columns = [
-            { type: 'checked', width: 60 },
+            { type: 'checkbox', width: 60 },
             { field: 'OrderID', headerText: 'Order ID', isPrimaryKey: true, textAlign: 'Right', width: 120, validationRules: { required: true } },
             { field: 'CustomerID', headerText: 'Customer ID', width: 120, editType: 'dropdownedit' },
             { field: 'Freight', headerText: 'Frieght', width: 120, format: 'C2', editType: 'numericedit', textAlign: 'Right' },
@@ -168,27 +163,50 @@ var enableChk = new ej.buttons.CheckBox({ label: 'Enable Checkbox column', chang
 } });
 enableChk.appendTo('#enableChk');
 
-var enablepersist = new ej.buttons.CheckBox({ change: function (args) {
-    grid.clearSelection();
-    grid.selectionSettings.persistSelection = args.checked;
-} });
-enablepersist.appendTo('#enablepersist');
-
-var enablecache = new ej.buttons.CheckBox({ change: function (args) {
+var enablecache = new ej.buttons.CheckBox({ label: 'Enable Cache', change: function (args) {
     grid.setProperties({
         filterSettings: {
             columns: []
         },
         groupSettings: { columns : [] },
         sortSettings: { columns: [] },
-        infiniteScrollSettings: { enablecache : args.checked }
+        infiniteScrollSettings: { enableCache : args.checked }
     }, true);
     grid.freezeRefresh();
-    // grid.infiniteScrollSettings.enablecache = args.checked;
+    // grid.infiniteScrollSettings.enableCache = args.checked;
 } });
 enablecache.appendTo('#enablecache');
 
-var enablelazyload = new ej.buttons.CheckBox({ change: function (args) {
+var remotedata = new ej.buttons.CheckBox({ label: 'Remote Data', change: function (args) {
+    grid.setProperties({
+        filterSettings: {
+            columns: []
+        },
+        groupSettings: { columns : [] },
+        sortSettings: { columns: [] },
+        dataSource: args.checked ? remoteData : window.orderData
+    }, true);
+    grid.freezeRefresh();
+    // grid.infiniteScrollSettings.enableCache = args.checked;
+} });
+remotedata.appendTo('#remotedata');
+
+var frozen = new ej.buttons.CheckBox({ label: 'Frozen', change: function (args) {
+    grid.setProperties({
+        filterSettings: {
+            columns: []
+        },
+        groupSettings: { columns : [] },
+        sortSettings: { columns: [] },
+        frozenRows: args.checked ? 2 : undefined,
+        frozenColumns: args.checked ? 2 : undefined
+    }, true);
+    grid.freezeRefresh();
+    // grid.infiniteScrollSettings.enableCache = args.checked;
+} });
+frozen.appendTo('#frozen');
+
+var enablelazyload = new ej.buttons.CheckBox({ label: 'Lazy Load', change: function (args) {
     grid.clearGrouping();
     grid.setProperties({
         filterSettings: {
